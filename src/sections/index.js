f0830dd49f7fcf8359c36ab93f677e84fa60baa5
Abdul Ahad <ahad55056@gmail.com>
2024-03-10 19:43:55 +0500

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Footer,
+  CustomerReviews,
+  SuperQuality,
+  Subscribe,
+};
